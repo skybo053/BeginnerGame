45223b395bb8c377d063ed9285cac2e75c14c9ff
Whit Jackman <whitjackman@gmail.com>
2016-09-17 11:58:33 -0400

    added more text output to main.java

--- EasierBeginnerGame/src/mypackage/Main.java
+++ EasierBeginnerGame/src/mypackage/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		
 		new GameWindow("Game 1.0.0", 800, 700);
 		 System.out.println("alsdgkjsadlgkjasdg");
+		 System.out.println("stuff");
+		 System.out.println("more stuff");
+		 System.out.println("blah blah");
+		 System.out.println("cooooooooooooooooool");
 	}
 	
 	
