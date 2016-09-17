1e5613fbe6c061ab07a5d6186ba306e5c8f4606d
Whit Jackman <whitjackman@gmail.com>
2016-09-17 11:44:30 -0400

    added text output in main file

--- EasierBeginnerGame/src/mypackage/Main.java
+++ EasierBeginnerGame/src/mypackage/Main.java
@@ -5,7 +5,11 @@ package mypackage;
 public class Main {
 
 	public static void main(String[] args) {
+		int array[] = {1,2,3};
+		
 		new GameWindow("Game 1.0.0", 800, 700);
+		System.out.println("msg in working branch");
+		System.out.println("another message");
 	}
 	
 	
