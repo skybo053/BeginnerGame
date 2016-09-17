e0d9a01a8217ba9306637fea904929ecabe86138
Whit Jackman <whitjackman@gmail.com>
2016-09-17 11:52:10 -0400

    test

--- EasierBeginnerGame/src/mypackage/Main.java
+++ EasierBeginnerGame/src/mypackage/Main.java
@@ -5,11 +5,9 @@ package mypackage;
 public class Main {
 
 	public static void main(String[] args) {
-		int array[] = {1,2,3};
 		
 		new GameWindow("Game 1.0.0", 800, 700);
-		System.out.println("msg in working branch");
-		System.out.println("another message");
+		 System.out.println("alsdgkjsadlgkjasdg");
 	}
 	
 	
